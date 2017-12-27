33cfd89072b57350e22eafad9f85a0fa38faed0c
R Shaw <rshaw@olivermattei.net>
2017-12-27 06:42:45 -0500

    Add save unit tests

--- app/simpleapp/tests/test_forms.py
+++ app/simpleapp/tests/test_forms.py
@@ -22,8 +22,6 @@ class EventFormTestCase(TestCase):
         self.yesterday = self.today - datetime.timedelta(hours=24)
         self.tomorrow = self.today + datetime.timedelta(hours=24)
         
-        self.form = EventForm()
-
         self.event = Event.objects.create(
                         title="Test Event",
                         time_start=self.tomorrow,
@@ -35,17 +33,21 @@ class EventFormTestCase(TestCase):
         Test that the title field is still in the form's fields
         
         """
-        self.assertIn('title', self.form.fields)
+        form = EventForm()
+
+        self.assertIn('title', form.fields)
 
     def test_fields_replaced(self):
         """
         Test that time_range replaces both time_start and time_end fields in the form 
         
         """
-        self.assertIn('time_range', self.form.fields)
+        form = EventForm()
+        
+        self.assertIn('time_range', form.fields)
 
-        self.assertNotIn('time_start', self.form.fields)
-        self.assertNotIn('time_end', self.form.fields)
+        self.assertNotIn('time_start', form.fields)
+        self.assertNotIn('time_end', form.fields)
 
     def test_with_existing_instance(self):
         """
@@ -59,4 +61,59 @@ class EventFormTestCase(TestCase):
                 self.event.time_start)
         self.assertEqual(form.fields['time_range'].fields[1].initial, 
                 self.event.time_end)
+
+    def test_save_commit(self):
+        """ Test saving a form to database """
+        time_range = '{} - {}'.format(
+                    localize_input(
+                            to_current_timezone(self.tomorrow), 
+                            DATETIME_FORMAT
+                        ),
+                    localize_input(
+                            to_current_timezone(self.tomorrow), 
+                            DATETIME_FORMAT
+                        )
+                )
+        data = {
+                'title': 'Test Event',
+                'time_range': time_range,
+            }
+
+        form = EventForm(data)
+
+        self.assertTrue(form.is_valid())
+
+        event = form.save()
+        self.assertIsNotNone(event)
+        
+        self.assertTrue(Event.objects.filter(pk=event.pk).exists())
+    
+    def test_save_no_commit(self):
+        """ Test saving a form without committing directly to database """
+        time_range = '{} - {}'.format(
+                    localize_input(
+                            to_current_timezone(self.tomorrow), 
+                            DATETIME_FORMAT
+                        ),
+                    localize_input(
+                            to_current_timezone(self.tomorrow), 
+                            DATETIME_FORMAT
+                        )
+                )
+        data = {
+                'title': 'Test Event',
+                'time_range': time_range,
+            }
+
+        form = EventForm(data)
+
+        self.assertTrue(form.is_valid())
+
+        event = form.save(commit=False)
+        self.assertIsNotNone(event)
+        self.assertFalse(Event.objects.filter(pk=event.pk).exists())
+
+        event.save() 
+        self.assertTrue(Event.objects.filter(pk=event.pk).exists())
+
 
